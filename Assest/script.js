89a8530755b0437f9e15679204f863e5c1d3823e
Keegan Micu <LemonTag73@gmail.com>
2024-04-13 19:44:12 -0400

    added a header and a search bar, the search bar doesn't work at the moment

---
+++ Assest/script.js
@@ -0,0 +1,7 @@
+let searchHistroy = []
+//these are varaibles
+//function is a piece of code to reused 
+//function premater is data passed through the function
+const weatherApiRootUrl = 'https://api.openweathermap.org';
+const weatherApiKey = ffc2ddad50cd4e8e7e41a599af869618;
+
